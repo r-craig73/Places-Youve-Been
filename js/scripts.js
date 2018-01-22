a80dae7519fba4e8c6501097cdd39ed5f4e27a6c
Guest User <Guest@Epicodus-10D.local>
2018-01-22 14:19:51 -0800

    add more inputs and detail section

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,40 @@
 //business logic
 
-// Each destination should be an object with multiple properties, like location, landmarks, time of year
-
-
-// Display properties when a user clicks on the place's name.
-
-function Places(place) {
+function Places(place, land, time, note) {
   this.place = place;
-}
-Places.prototype.newPlace = function() {
-  return this.place + " ";
+  this.land = land;
+  this.time = time;
+  this.note = note;
 }
 
+var allNotes = new Places();
+Places.prototype.newPlace = function() {
+  return this.place + this.land + this.time + this.note;
+}
 
 // user interface logic
 $(document).ready(function() {
   $("form#placeInput").submit(function(event) {
       event.preventDefault();
 
-var inputtedNewPlace = $("input#newPlace").val();
+      var inputtedNewPlace = $("input#newPlace").val();
+      var inputtedLandmarks = $("input#newLandmarks").val();
+      var inputtedTime = $("input#newTime").val();
+      var inputtedNotes = $("input#newNotes").val();
 
-$("ul#places").append("<li><span class='place'>" + inputtedNewPlace + "</span></li>")
-$("#places").show();
+var allNotes = new Places(inputtedNewPlace, inputtedLandmarks, inputtedTime, inputtedNotes);
+
+$("ul#places").append("<li><span class='place'>" + allNotes.place + "</span></li>");
+// $("#places").show();
+
+
+$(".place").last().click(function() {
+$("#showDetails").show();
+$(".landmark").text(allNotes.land);
+$(".year").text(allNotes.time);
+$(".notes").text(allNotes.note);
+});
+
+
 });
 });
--- readme.md
+++ readme.md
@@ -7,16 +7,22 @@
 
 
 ## Specifications
-### User input properties and save the results in an object
+### User input properties and save the properties into an object
 #### Input: New York City, Wall Street, 1998, three day trip in NYC
-#### Output: var places = {location: "New York City", landmarks: ["Wall Street", "Statue of Liberty", "Appolo Theater"], time: 1998, notes: "3 day trip"};
+#### Output: function Places (location, landmark, time, notes) {
+####            this.place = place;
+####            this.land = land;
+####            this.time = time;
+####            this.note = note;
+####         }
+####
 
-### Display location when the user presses the "New Place" Button
-#### Input: Users presses "New Place"
+### Display location only when the user presses the "New Place" Button
+#### Input: Users presses "Add"
 #### Output: New York City
 
 ### Display properties when a user click on the location
-#### Input: User clicks "New York City"
+#### Input: User clicks the phrase "New York City"
 #### Output: <li>Landmarks: Wall Street, Statue of Liberty....</li>
 ####         <li>Time: 1998</li>
 ####        <li>Notes: 3 day trip</li>
